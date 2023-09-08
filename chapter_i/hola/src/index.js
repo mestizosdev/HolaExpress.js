ff6c4c28428726cd2e6d3694dfbc6d7c07642f08
Jorge Luis <jorgequiguango@outlook.com>
2023-09-08 18:54:32 -0500

    [FIX] Message

--- chapter_i/hola/src/index.js
+++ chapter_i/hola/src/index.js
@@ -17,6 +17,6 @@ const PORT = process.env.PORT
 
 server.listen(PORT, () => {
   console.log(
-    `Server running on port ${PORT}`
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
   )
 })
