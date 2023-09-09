387733289f245397eb36c0fe2c75951aec159c49
Jorge Luis <jorgequiguango@outlook.com>
2023-09-09 14:41:19 -0500

    [FIX] Logger

--- chapter_ii/hola/doc/api/httpie.md
+++ chapter_ii/hola/doc/api/httpie.md
@@ -2,3 +2,7 @@
 ```
 http localhost:3000/ping
 ```
+## GET version
+```
+http localhost:3000/version
+```
--- chapter_ii/hola/src/modules/version/controllers/version.js
+++ chapter_ii/hola/src/modules/version/controllers/version.js
@@ -1,4 +1,7 @@
 const versionService = require('../services/version')
+const os = require('os')
+const pkg = require('../../../../package.json')
+const ErrorMessage = require('../../../utils/error-message')
 
 exports.getVersion = async (req, res) => {
   try {
@@ -7,11 +10,15 @@ exports.getVersion = async (req, res) => {
     res.json({
       application: {
         name: 'HolaExpress.js',
+        author: pkg.author,
+        version: pkg.version,
+        versionOS: os.platform() + ' ' + os.release() + ' ' + os.arch(),
         versionDatabase: version.version_database,
         versionNode: process.version
       }
     })
-  } catch (error) {
+  } catch (e) {
+    const error = new ErrorMessage(e.message, e.content, __filename)
     return res.status(503).json(
       error.message
     )
