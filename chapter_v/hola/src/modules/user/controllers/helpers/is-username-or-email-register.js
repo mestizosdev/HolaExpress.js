69833d22999fc65d0f602cfb85f6e4d94004067b
Jorge Luis <jorgequiguango@outlook.com>
2023-09-11 13:48:51 -0500

    [IMP] Promise allSettled

--- chapter_v/hola/src/modules/user/controllers/helpers/is-username-or-email-register.js
+++ chapter_v/hola/src/modules/user/controllers/helpers/is-username-or-email-register.js
@@ -24,3 +24,34 @@ async function isUsernameOrEmailRegister (username, email) {
 }
 
 module.exports.isUsernameOrEmailRegister = isUsernameOrEmailRegister
+/*
+// Promise.allSettled -> wait for resolve all promises
+async function isUsernameOrEmailRegister (username, email) {
+  const start = performance.now()
+
+  const existRecord = await Promise.allSettled([
+    userService.getByUsername(username),
+    userService.getByEmail(email)
+  ]).then(values => {
+    console.log(values)
+    if (values[0].value) {
+      return {
+        exist: true,
+        message: `The username ${username} already exist`
+      }
+    } else if (values[1].value) {
+      return {
+        exist: true,
+        message: `The email ${email} already exist`
+      }
+    } else {
+      return { exist: false, message: '' }
+    }
+  })
+
+  const end = performance.now()
+  console.log(`Call to execute isUsernameOrEmailRegister took ${end - start} milliseconds`)
+
+  return existRecord
+}
+*/
